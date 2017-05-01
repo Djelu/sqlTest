39bc60d02d32b8bac96e202a89e86109d8ee1d96
Djelu <djelu333@gmail.com>
2017-05-01 11:47:29 +0300

    Ready

--- src/DB.java
+++ src/DB.java
@@ -2,21 +2,153 @@ import java.sql.*;
 
 public class DB {
     static DB db;
-    Connection connection;
-    Statement statement;
-    ResultSet resultSet;
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    final int COLUMNS_WIDTH = 15;
 
     public static void main(String[] args){
         db = new DB();
-        try{
         db.connect();
-        }catch (ClassNotFoundException|SQLException e) { e.printStackTrace();}
-
-
+        for(int i = 1; i<=5; i++) db.tasksExecution(i);
+        db.disconnect();
     }
 
-    void connect() throws ClassNotFoundException, SQLException {
-        Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection("jdbc:sqlite:F:/programming/db/sqlite/db.s3db");
+    private void disconnect(){
+        try {
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        }catch (SQLException e) {e.printStackTrace();}
+    }
+
+    void connect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String currentDir = System.getProperty("user.dir");
+            connection = DriverManager.getConnection("jdbc:sqlite:"+currentDir+"\\db.s3db");
+
+        } catch (SQLException | ClassNotFoundException e) {e.printStackTrace();}
+    }
+
+    void tasksExecution(int num){
+        try {
+            statement = connection.createStatement();
+            switch (num){
+                case 1:{
+                    //1)Вывести фамилии всех сотрудников и отделы в которых они работают.
+                    resultSet = statement.executeQuery(
+                        "SELECT EMP.ENAME,DEPT.DNAME    \n"+
+                        "FROM   EMP,DEPT                \n"+
+                        "WHERE  EMP.DEPTNO=DEPT.DEPTNO;"
+                    );
+                }break;
+                case 2:{
+                    //2)Вывести фамилии, должности и зарплаты сотрудников, которым не положены комисcионные.
+                    resultSet = statement.executeQuery(
+                        "SELECT ENAME, JOB, SAL \n"+
+                        "FROM EMP               \n"+
+                        "WHERE COMM IS NULL;"
+                    );
+                }break;
+                case 3:{
+                    //3)Вывести количество сотрудников и среднюю зарплату в 20 отделе.
+                    resultSet = statement.executeQuery(
+                        "SELECT count(ENAME) AS EMP_COUNT, avg(SAL) AS SAL_AVG  \n"+
+                        "FROM EMP                                               \n"+
+                        "WHERE DEPTNO=20;"
+                    );
+                }break;
+                case 4:{
+                    //4)Вывести отделы в которых больше 3-х сотрудников.
+                    ////Посчитать количество сотрудников в таких отделах.
+                    resultSet = statement.executeQuery(
+                        "SELECT DEPT.DNAME, count(EMP.DEPTNO) AS DNAME_COUNT    \n"+
+                        "FROM EMP,DEPT                                          \n"+
+                        "WHERE EMP.DEPTNO=DEPT.DEPTNO                           \n"+
+                        "GROUP BY DEPT.DNAME                                    \n"+
+                        "HAVING count(EMP.DEPTNO)>3;"
+                    );
+                }break;
+                case 5:{
+                    //5)Выведите фамилии сотрудников, название их отделов,
+                    //города расположения отделов, зарплату сотрудников и уровень их зарплаты.
+                    //Отсортировать результат в порядке возрастания уровня зарплаты,
+                    //если уровень зарплаты совпадает, то по зарплате.
+                    resultSet = statement.executeQuery(
+                        "SELECT EMP.ENAME,DEPT.DNAME,DEPT.LOC,EMP.SAL,t.grade AS GRADE                  \n"+
+                        "FROM EMP,DEPT,(SELECT EMP.SAL AS sal,SALGRADE.GRADE AS grade                   \n"+
+                        "               FROM EMP,SALGRADE                                               \n"+
+                        "               WHERE EMP.SAL BETWEEN SALGRADE.LOSAL AND SALGRADE.HISAL) AS t   \n"+
+                        "WHERE EMP.DEPTNO=DEPT.DEPTNO AND EMP.SAL=t.sal                                 \n"+
+                        "GROUP BY EMP.ENAME,DEPT.DNAME,DEPT.LOC,EMP.SAL,GRADE                           \n"+
+                        "ORDER BY GRADE ASC, EMP.SAL ASC"
+                    );
+                }break;
+            }
+            if (resultSet!=null) printResult(resultSet,num);
+
+        } catch (SQLException e) { e.printStackTrace();}
+    }
+
+    private void printResult(ResultSet resultSet, int taskNum) throws SQLException {
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+        int colsCount = resultSetMetaData.getColumnCount();
+        int[] colsTypes = new int[colsCount];
+        StringBuilder sb = new StringBuilder();
+        sb.append("Task number ").append(taskNum).append(":\n\n");
+
+        for(int i=0; i<colsCount; i++){
+            sb.append(normalize(resultSetMetaData.getColumnName(i+1),COLUMNS_WIDTH));//приводя к выбранной ширине, записываем имена столбцов
+            colsTypes[i] = resultSetMetaData.getColumnType(i+1);
+        }
+        sb.append("\n");
+
+        for(int i=0; i<colsCount*COLUMNS_WIDTH; i++) sb.append("-");
+        sb.append("\n");
+
+        while (resultSet.next()){
+            //записываем строки
+            for(int i=0,intCell; i<colsCount; i++){
+                String sCell = "";
+                switch (colsTypes[i]){
+                    case 4:{//INTEGER
+                        intCell= resultSet.getInt(i+1);
+                        sCell = resultSet.wasNull() ?"NULL" :String.valueOf(intCell);
+                    }break;
+                    case 12:{//VARCHAR
+                        sCell = resultSet.getString(i+1);
+                        if(resultSet.wasNull()) sCell = "NULL";
+                    }break;
+                    case 91:{//DATE
+                        sCell = resultSet.getString(i+1);
+                        if(resultSet.wasNull()) sCell = "NULL";
+                    }break;
+                }
+                sb.append(normalize(sCell,COLUMNS_WIDTH));//приводим к выбранной ширине
+            }
+            sb.append("\n");
+        }
+        sb.append("\n");
+        System.out.println(sb.toString());
+    }
+
+    private String normalize(String string, int n){
+        int ln = string.length();
+        int newLn = ln<=n ?n :ln;
+        int q1 = (newLn-ln)/2;
+        int q2 = newLn-q1-ln;
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2;
+        for(int i=0; i<q1; i++) sb1.append(" ");
+        if(q1==q2)
+            sb2 = sb1;
+        else{
+            sb2 = new StringBuilder();
+            for(int i=0; i<q2; i++) sb2.append(" ");
+        }
+        return sb1.toString() + string + sb2.toString();
     }
 }
--- src/query.sql
+++ src/query.sql
@@ -1,129 +1,82 @@
--- --Вставка в EMP
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7369, 'SMITH',    'CLERK', 7902, '12-17-1980',  800, NULL, 20);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7499, 'ALLEN', 'SALESMAN', 7698, '02-20-1981', 1600,  300, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7521,  'WARD', 'SALESMAN', 7698, '02-22-1981', 1250,  500, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7566, 'JONES',  'MANAGER', 7839, '03-02-1981', 2975, NULL, 20);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7654,'MARTIN', 'SALESMAN', 7698, '09-28-1981', 1250, 1400, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7698, 'BLAKE',  'MANAGER', 7839, '05-01-1981', 2850, NULL, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7782, 'CLARK',  'MANAGER', 7839, '06-09-1981', 2450, NULL, 10);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7788, 'SCOTT',  'ANALYST', 7566, '12-01-1982', 3000, NULL, 20);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7839,  'KING','PRESIDENT', NULL, '11-17-1981', 5000, NULL, 10);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7844,'TURNER', 'SALESMAN', 7698, '09-08-1981', 1500,    0, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7876, 'ADAMS',    'CLERK', 7788, '01-12-1983', 1100, NULL, 20);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7900, 'JAMES',    'CLERK', 7698, '12-03-1981',  950, NULL, 30);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7902,  'FORD',  'ANALYST', 7566, '12-03-1981', 3000, NULL, 20);
--- INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7934,'MILLER',    'CLERK', 7782, '01-23-1983', 1300, NULL, 10);
---
--- --Вставка в DEPT
--- INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (10, 'ACCOUNTING', 'NEW YORK');
--- INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (20, 'RESEARCH', 'DALLAS');
--- INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (30, 'SALES', 'CHICAGO');
--- INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (40, 'OPERATIONS', 'BOSTON');
---
--- --Вставка в SALGRADE
--- INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (1,  700, 1200);
--- INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (2, 1201, 1400);
--- INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (3, 1401, 2000);
--- INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (4, 2001, 3000);
--- INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (5, 3001, 9999);
+CREATE TABLE EMP(
+  EMPNO    INTEGER     PRIMARY KEY,
+  ENAME    VARCHAR(20) NOT NULL   ,
+  JOB      VARCHAR(20) NOT NULL   ,
+  MGR      INTEGER     NULL       ,
+  HIREDATE DATE        NOT NULL   ,
+  SAL      INTEGER     NOT NULL   ,
+  COMM     INTEGER     NULL       ,
+  DEPTNO   INTEGER     NOT NULL
+);
 
---1.Вывести фамилии всех сотрудников и отделы в которых они работают.
--- SELECT "EMP"."ENAME", "DEPT"."DNAME"
--- FROM "EMP","DEPT"
--- WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
+CREATE TABLE DEPT(
+  DEPTNO    INTEGER     PRIMARY KEY,
+  DNAME     VARCHAR(20) NOT NULL   ,
+  LOC       VARCHAR(20) NOT NULL
+);
 
---2.Вывести фамилии, должности и зарплаты сотрудников, которым не положены комисcионные.
--- SELECT "ENAME", "JOB", "SAL"
--- FROM "EMP"
--- WHERE "COMM" IS NULL
+CREATE TABLE SALGRADE(
+  GRADE    INTEGER     PRIMARY KEY,
+  LOSAL    VARCHAR(20) NOT NULL   ,
+  HISAL    VARCHAR(20) NOT NULL
+);
 
---3.Вывести количество сотрудников и среднюю зарплату в 20 отделе.
--- SELECT count("ENAME") as "EMP_COUNT", avg("SAL") as "SAL_AVG"
--- FROM "EMP"
--- WHERE "DEPTNO"=20
+--Вставка в EMP
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7369, 'SMITH',    'CLERK', 7902, '12-17-1980',  800, NULL, 20);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7499, 'ALLEN', 'SALESMAN', 7698, '02-20-1981', 1600,  300, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7521,  'WARD', 'SALESMAN', 7698, '02-22-1981', 1250,  500, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7566, 'JONES',  'MANAGER', 7839, '03-02-1981', 2975, NULL, 20);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7654,'MARTIN', 'SALESMAN', 7698, '09-28-1981', 1250, 1400, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7698, 'BLAKE',  'MANAGER', 7839, '05-01-1981', 2850, NULL, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7782, 'CLARK',  'MANAGER', 7839, '06-09-1981', 2450, NULL, 10);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7788, 'SCOTT',  'ANALYST', 7566, '12-01-1982', 3000, NULL, 20);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7839,  'KING','PRESIDENT', NULL, '11-17-1981', 5000, NULL, 10);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7844,'TURNER', 'SALESMAN', 7698, '09-08-1981', 1500,    0, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7876, 'ADAMS',    'CLERK', 7788, '01-12-1983', 1100, NULL, 20);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7900, 'JAMES',    'CLERK', 7698, '12-03-1981',  950, NULL, 30);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7902,  'FORD',  'ANALYST', 7566, '12-03-1981', 3000, NULL, 20);
+INSERT INTO "EMP" ("EMPNO", "ENAME", "JOB", "MGR", "HIREDATE", "SAL", "COMM", "DEPTNO") VALUES (7934,'MILLER',    'CLERK', 7782, '01-23-1983', 1300, NULL, 10);
 
---4.Вывести отделы в которых больше 3-х сотрудников. Посчитать количество сотрудников в таких отделах.
--- SELECT "DEPT"."DNAME", count("EMP"."DEPTNO") as "DNAME_COUNT"
--- FROM "EMP","DEPT"
--- WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
--- GROUP BY "DEPT"."DNAME"
--- HAVING count("EMP"."DEPTNO")>3
+--Вставка в DEPT
+INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (20, 'RESEARCH', 'DALLAS');
+INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (30, 'SALES', 'CHICAGO');
+INSERT INTO "DEPT" ("DEPTNO", "DNAME", "LOC") VALUES (40, 'OPERATIONS', 'BOSTON');
 
---5.Выведите фамилии сотрудников, название их отделов, города расположения отделов, зарплату сотрудников и уровень их зарплаты.
---Отсортировать результат в порядке возрастания уровня зарплаты, если уровень зарплаты совпадает, то по зарплате.
--- SELECT "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL"
--- FROM "EMP","DEPT","SALGRADE"
---
--- SELECT "SALGRADE"."GRADE"
--- FROM "EMP","SALGRADE"
--- WHERE "SALGRADE"."LOSAL">="EMP"."SAL" OR "SALGRADE"."HISAL"<="EMP"."SAL"
+--Вставка в SALGRADE
+INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (1,  700, 1200);
+INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (2, 1201, 1400);
+INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (3, 1401, 2000);
+INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (4, 2001, 3000);
+INSERT INTO "SALGRADE" ("GRADE", "LOSAL", "HISAL") VALUES (5, 3001, 9999);
 
+-- 1.Вывести фамилии всех сотрудников и отделы в которых они работают.
+SELECT "EMP"."ENAME", "DEPT"."DNAME"
+FROM "EMP","DEPT"
+WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO";
 
--- SELECT * FROM
--- (SELECT "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC"
--- FROM "EMP","DEPT","SALGRADE"
--- WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
---
--- UNION
---
--- SELECT "EMP"."SAL","SALGRADE"."GRADE"
--- FROM "EMP","SALGRADE"
--- WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as alias
--- GROUP BY "EMP"."ENAME"
+-- 2.Вывести фамилии, должности и зарплаты сотрудников, которым не положены комисcионные.
+SELECT "ENAME", "JOB", "SAL"
+FROM "EMP"
+WHERE "COMM" IS NULL;
 
--- Select * from (SELECT "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL" as sal
---                FROM "EMP","DEPT","SALGRADE"
---                WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
---                GROUP BY "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL") as t1
---   LEFT JOIN (SELECT "EMP"."SAL" as sal,"SALGRADE"."GRADE"
---              FROM "EMP","SALGRADE"
---              WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t2 ON t1.sal=t2.sal
+-- 3.Вывести количество сотрудников и среднюю зарплату в 20 отделе.
+SELECT count("ENAME") as "EMP_COUNT", avg("SAL") as "SAL_AVG"
+FROM "EMP"
+WHERE "DEPTNO"=20;
 
+-- 4.Вывести отделы в которых больше 3-х сотрудников. Посчитать количество сотрудников в таких отделах.
+SELECT "DEPT"."DNAME", count("EMP"."DEPTNO") as "DNAME_COUNT"
+FROM "EMP","DEPT"
+WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
+GROUP BY "DEPT"."DNAME"
+HAVING count("EMP"."DEPTNO")>3;
 
--- Select * from (SELECT "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL" as sal
---                FROM "EMP","DEPT","SALGRADE"
---                WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
---                GROUP BY "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL") as t1
---   LEFT JOIN (SELECT "EMP"."SAL" as sal,"SALGRADE"."GRADE"
---              FROM "EMP","SALGRADE"
---              WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t2 ON t1.sal=t2.sal
---
---
-
-
---
---
--- Select * from (SELECT "EMP"."ENAME" as ename,"DEPT"."DNAME" as dname,"DEPT"."LOC" as loc,"EMP"."SAL" as sal
---                FROM "EMP","DEPT","SALGRADE"
---                WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
---                GROUP BY "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL") as t1
---   LEFT JOIN (SELECT "EMP"."SAL" as sal,"SALGRADE"."GRADE" as grade
---              FROM "EMP","SALGRADE"
---              WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t2 ON t1.sal=t2.sal
--- GROUP BY t1.ename, t1.dname, t1.loc, t1.sal, t2.sal, t2.grade
---
-
---
--- Select t1.ename,t1.dname, t1.loc, t1.sal, t2.grade from (SELECT "EMP"."ENAME" as ename,"DEPT"."DNAME" as dname,"DEPT"."LOC" as loc,"EMP"."SAL" as sal
---               FROM "EMP","DEPT","SALGRADE"
---               WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
---               GROUP BY "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL") as t1,
---   (SELECT "EMP"."SAL" as sal,"SALGRADE"."GRADE" as grade
---    FROM "EMP","SALGRADE"
---    WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t2
--- GROUP BY t1.ename,t1.dname, t1.loc, t1.sal, t2.grade
---
---
-
-
-SELECT t1.ename,t1.dname, t1.loc, t1.sal, t2.grade
-FROM
-  (SELECT "EMP"."ENAME" as ename,"DEPT"."DNAME" as dname,"DEPT"."LOC" as loc, "EMP"."SAL" as sal
-   FROM "EMP","DEPT","SALGRADE"
-   WHERE "EMP"."DEPTNO"="DEPT"."DEPTNO"
-   GROUP BY "EMP"."ENAME","DEPT"."DNAME","DEPT"."LOC","EMP"."SAL") as t1,
-  (SELECT "SALGRADE"."GRADE" as grade
-   FROM "EMP","SALGRADE"
-   WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t2
-GROUP BY t1.ename,t1.dname, t1.loc, t1.sal, t2.grade
-
+-- 5.Выведите фамилии сотрудников, название их отделов, города расположения отделов, зарплату сотрудников и уровень их зарплаты.
+-- Отсортировать результат в порядке возрастания уровня зарплаты, если уровень зарплаты совпадает, то по зарплате.
+SELECT EMP.ENAME,DEPT.DNAME,DEPT.LOC,EMP.SAL,t.grade as GRADE
+FROM EMP,DEPT,(SELECT "EMP"."SAL" as sal,"SALGRADE"."GRADE" as grade
+               FROM "EMP","SALGRADE"
+               WHERE "EMP"."SAL" BETWEEN "SALGRADE"."LOSAL" AND "SALGRADE"."HISAL") as t
+WHERE EMP.DEPTNO=DEPT.DEPTNO AND EMP.SAL=t.sal
+GROUP BY EMP.ENAME,DEPT.DNAME,DEPT.LOC,EMP.SAL,t.grade
